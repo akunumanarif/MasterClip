c2eafb7e8481fac36a10b0fc826030120b313e71
Nu'man Arif <numan.arif@ikonsultan>
2026-02-14 23:53:00 +0700

    Dockerize

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,11 +5,11 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:8000/api/:path*",
+        destination: `${process.env.BACKEND_URL || "http://127.0.0.1:8000"}/api/:path*`,
       },
       {
         source: "/output/:path*",
-        destination: "http://127.0.0.1:8000/output/:path*",
+        destination: `${process.env.BACKEND_URL || "http://127.0.0.1:8000"}/output/:path*`,
       },
     ];
   },
